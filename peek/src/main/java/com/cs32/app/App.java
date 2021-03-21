6b123fb08c5999867992d9848ddd290016cb01a5
elliotjinoohong <elliot_hong@brown.edu>
2021-03-21 18:49:12 -0400

    working mvn set up but dependencies not working

---
+++ peek/src/main/java/com/cs32/app/App.java
@@ -0,0 +1,14 @@
+package com.cs32.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println("Making new Main");
+        new Main(args).run();
+    }
+
+}
