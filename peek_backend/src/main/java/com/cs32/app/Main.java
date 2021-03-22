38a04dec8c0b047cf8f59675a15748ac9a7a3d90
Xinzhe Chai <xinzhe_chai@brown.edu>
2021-03-22 13:35:43 -0400

    completed mongodb api flow

--- peek_backend/src/main/java/com/cs32/app/Main.java
+++ peek_backend/src/main/java/com/cs32/app/Main.java
@@ -26,5 +26,6 @@ public class Main {
 //    Spark.externalStaticFileLocation("src/main/resources/static");
 //    Spark.exception(Exception.class, new ExceptionPrinter());
     Spark.post("/get-suggested-polls", new GetSuggestedPollsHandler());
+    Spark.post("/user/new", new AddUserHandler());
   }
 }
--- peek_backend/src/main/java/com/cs32/app/database/Connection.java
+++ peek_backend/src/main/java/com/cs32/app/database/Connection.java
@@ -1,36 +1,41 @@
 package com.cs32.app.database;
 
-import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.net.UnknownHostException;
-
 public class Connection {
 
   public static MongoClient mongoClient;
   public static MongoDatabase mongoDatabase;
+  public static MongoCollection<Document> userCollection;
 
   public Connection() {
     String connectionString = System.getProperty("mongodb.uri");
     mongoClient = MongoClients.create(connectionString);
     mongoDatabase = mongoClient.getDatabase("main");
-    dummyQuery();
+    userCollection = mongoDatabase.getCollection("user");
+    dummyGetQuery();
   }
 
-  public static void dummyQuery() {
-    MongoCollection<Document> collection = mongoDatabase.getCollection("user");
-    System.out.println(collection);
+  public static void dummyAddQuery(String password) {
+
     System.out.println("adding new document");
     try {
-      Document user = new Document("_id", "jeeesdfsasaeo1sdf11");
-      user.append("name", "Jo Bloggs");
-      collection.insertOne(user);
+      Document user = new Document("_id", (int) (Math.random() * 100000));
+      user.append("password", password);
+      userCollection.insertOne(user);
     } catch (Exception e) {
       System.out.println("failed");
     }
+    System.out.println("success in insertion");
+
+  }
+
+  public static void dummyGetQuery() {
+    Document user = userCollection.find(new Document("_id", 10003)).first();
+    System.out.println("User 1: " + user.toJson());
   }
 }
