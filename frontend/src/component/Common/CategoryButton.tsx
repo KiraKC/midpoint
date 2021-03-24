b9f6bb15803d7a1e7426e9fc2551af93b6e56aa0
Xinzhe Chai <xinzhe_chai@brown.edu>
2021-03-24 04:07:36 -0400

    buggy code

--- frontend/src/component/Common/CategoryButton.tsx
+++ frontend/src/component/Common/CategoryButton.tsx
@@ -5,16 +5,49 @@ import '../../styles/Common/CategoryButton.css'
 interface ICategoryButtonProps {
 	emoji: string,
 	text: string,
-	highlightColor: string
+	highlightColor: string,
+	categories: string[],
+	setCategories: any
 }
 
 function CategoryButton(props: ICategoryButtonProps) {
 
 	const [selected, setSelected]: [boolean, any] = useState(false)
+
+	const selectedStyle = {
+		backgroundColor: props.highlightColor,
+		color: 'white',
+		border: '2px solid',
+		borderColor: props.highlightColor
+	};
+
+	const defaultStyle = {
+		backgroundColor: 'transparent'
+	}
+
+	const handleSelect = () => {
+		let tempCategories;
+		if (!selected) {
+			tempCategories = props.categories;
+			tempCategories.push(props.text)
+			props.setCategories(tempCategories)
+		} else {
+			const index = props.categories.indexOf(props.text);
+			if (index > -1) {
+				tempCategories = props.categories;
+				tempCategories.splice(index, 1);
+			}
+			props.setCategories(tempCategories);
+		}
+		console.log(tempCategories)
+		setSelected(!selected);
+	}
 	return (
-		<button className="category-button">
-			<div style={{marginTop: '3px'}}><Emoji emoji={props.emoji} set='apple' size={16} /></div>
-			<div style={{marginLeft: '5px'}}>{props.text}</div>
+		<button className="category-button"
+			style={selected ? selectedStyle : defaultStyle}
+			onClick={() => handleSelect()}>
+			<div style={{ marginTop: '3px' }}><Emoji emoji={props.emoji} set='apple' size={16} /></div>
+			<div style={{ marginLeft: '5px' }}>{props.text}</div>
 		</button>
 	);
 }
--- frontend/src/component/Common/NewPollModal.tsx
+++ frontend/src/component/Common/NewPollModal.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import '../../styles/Common/NewPollModal.css'
 import CategoryButton from './CategoryButton';
 import 'emoji-mart/css/emoji-mart.css'
-import { BaseEmoji, Emoji, Picker } from 'emoji-mart'
+import { Emoji, Picker } from 'emoji-mart'
 import { useState } from 'react';
 
 interface INewPollModal {
@@ -32,15 +32,25 @@ function NewPollModal(props: INewPollModal) {
 
 	Modal.setAppElement("#root")
 
-	const [emoji, setEmoji]: [string, any] = useState('watermelon');
+	const [pollEmojiArray, setPollEmojiArray]: [string[], any] = useState(['watermelon', 'sad', 'sad']);
+	const [isEmojiOpenArray, setIsEmojiOpenArray]: [boolean[], any] = useState([false, false, false]);
 	const [question, setQuestion]: [string, any] = useState('');
-	const [isEmojiOpen, setIsEmojiOpen]: [boolean, any] = useState(false);
 	const [categories, setCategories]: [string[], any] = useState([])
+	
+	const handlePollEmoji = (emoji: any, index: number) => {
+		let tempEmojiArray = pollEmojiArray;
+		tempEmojiArray[index] = emoji.id;
+		setPollEmojiArray(tempEmojiArray);
 
-	const handleEmoji = (emoji: any) => {
-		setEmoji(emoji.id);
-		console.log(emoji.id)
-		setIsEmojiOpen(false)
+		let tempEmojiOpenArray = isEmojiOpenArray;
+		tempEmojiOpenArray[index] = false;
+		setIsEmojiOpenArray(tempEmojiOpenArray)
+	}
+
+	const handleEmojiOpen = (index: number) => {
+		let tempEmojiOpenArray = isEmojiOpenArray;
+		tempEmojiOpenArray[index] = !tempEmojiOpenArray[index];
+		setIsEmojiOpenArray(tempEmojiOpenArray)
 	}
 
 	return (
@@ -58,21 +68,42 @@ function NewPollModal(props: INewPollModal) {
 					</button>
 				</div>
 				<div className="poll-modal-input-module display-relative">
-					<button className="emoji-picker-button" onClick={() => setIsEmojiOpen(!isEmojiOpen)}><Emoji emoji={emoji} set='apple' size={25} /></button>
-					{isEmojiOpen ? <div className="emoji-picker"><Picker title='Pick your emoji…' emoji='point_up' onClick={(emoji, event) => handleEmoji(emoji)} /></div> : ''}
+					<button className="emoji-picker-button" onClick={() => handleEmojiOpen(0)}><Emoji emoji={pollEmojiArray[0]} set='apple' size={25} /></button>
+					{isEmojiOpenArray ? <div className="emoji-picker"><Picker title='Pick your emoji…' emoji='point_up' onClick={(emoji, event) => handlePollEmoji(emoji, 0)} /></div> : ''}
 					<div className="poll-modal-question-desc-emoji">EMOJI</div>
 					<input className="poll-modal-question-input" type="text"
 						placeholder="What's on your curious mind?"></input>
 					<div className="poll-modal-question-desc-question">QUESTION</div>
 				</div>
 
+				<div className="poll-modal-input-module display-relative">
+					<button className="emoji-picker-button" onClick={() => handleEmojiOpen(1)}><Emoji emoji={pollEmojiArray[1]} set='apple' size={25} /></button>
+					{isEmojiOpenArray ? <div className="emoji-picker"><Picker title='Pick your emoji…' emoji='point_up' onClick={(emoji, event) => handlePollEmoji(emoji, 1)} /></div> : ''}
+					<div className="poll-modal-question-desc-emoji">EMOJI</div>
+					<input className="poll-modal-question-input" type="text"
+						placeholder="What's on your curious mind?"></input>
+					<div className="poll-modal-question-desc-question">ANSWER 1</div>
+				</div>
+
+				<div className="poll-modal-input-module display-relative">
+					<button className="emoji-picker-button" onClick={() => handleEmojiOpen(2)}><Emoji emoji={pollEmojiArray[2]} set='apple' size={25} /></button>
+					{isEmojiOpenArray ? <div className="emoji-picker"><Picker title='Pick your emoji…' emoji='point_up' onClick={(emoji, event) => handlePollEmoji(emoji, 2)} /></div> : ''}
+					<div className="poll-modal-question-desc-emoji">EMOJI</div>
+					<input className="poll-modal-question-input" type="text"
+						placeholder="What's on your curious mind?"></input>
+					<div className="poll-modal-question-desc-question">ANSWER 2</div>
+				</div>
+
 				<div className="poll-modal-input-module display-flex">
-					<CategoryButton emoji="cop" text="Sports" highlightColor={"black"}/>
-					<CategoryButton emoji="cop" text="Sports" highlightColor={"black"}/>
-					<CategoryButton emoji="cop" text="Sports" highlightColor={"black"}/>
-					<CategoryButton emoji="cop" text="Sports" highlightColor={"black"}/>
-					<CategoryButton emoji="cop" text="Sports" highlightColor={"black"}/>
-					<CategoryButton emoji="cop" text="Sports" highlightColor={"black"}/>
+					<CategoryButton emoji="basketball" text="sports" highlightColor={"#74AEBB"} categories={categories} setCategories={setCategories}/>
+					<CategoryButton emoji="mega" text="politics" highlightColor={"#D83282"} categories={categories} setCategories={setCategories}/>
+					<CategoryButton emoji="joy" text="funny" highlightColor={"#1D5110"} categories={categories} setCategories={setCategories}/>
+					<CategoryButton emoji="classical_building" text="culture" highlightColor={"#2ABC88"} categories={categories} setCategories={setCategories}/>
+					<CategoryButton emoji="dancer" text="entertainment" highlightColor={"#905A00"} categories={categories} setCategories={setCategories}/>
+					<CategoryButton emoji="hamburger" text="food" highlightColor={"#494848"} categories={categories} setCategories={setCategories}/>
+					<CategoryButton emoji="school_satchel" text="education" highlightColor={"#F24343"} categories={categories} setCategories={setCategories}/>
+					<CategoryButton emoji="thinking_face" text="serious" highlightColor={"#B4154E"} categories={categories} setCategories={setCategories}/>
+					<CategoryButton emoji="cupid" text="relationship" highlightColor={"#C18FD2"} categories={categories} setCategories={setCategories}/>
 				</div>
 			</Modal>
 		</div>
