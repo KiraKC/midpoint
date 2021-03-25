d275d6245899f3354bed2e2ac04a49abaa9ace60
Xinzhe Chai <xinzhe_chai@brown.edu>
2021-03-25 01:38:35 -0400

    temp update

--- frontend/src/component/Common/Header.tsx
+++ frontend/src/component/Common/Header.tsx
@@ -7,7 +7,7 @@ interface IHeaderProps {
 function Header(props: IHeaderProps) {
 	return (
 		<div className="header">
-			<a className="header-link" href="./app.tsx" target="_blank">Peak.io</a>
+			<a className="header-link" href="./app.tsx">Peak.io</a>
 			<div>
 				<button className="nav-link">Log in</button>
 				<button className="nav-link" onClick={() => props.setIsModalOpen(true)}>New Poll</button>
